7ee4224701da1493311cfa2963742fb4a02e3b55
Nathan Brasher <nfbrasher@stripe.com>
2025-12-06 14:46:54 -0600

    feat: day 6 touchup

--- 6/main.py
+++ 6/main.py
@@ -1,5 +1,4 @@
 import re
-from copy import deepcopy
 
 from common import run
 
@@ -7,33 +6,31 @@ from common import run
 def main(inp: str):
     # Parse inputs
     data = inp.splitlines()
-    operators = re.sub(r'\s+', ' ', data[-1]).split(' ')
+    operators = re.sub(r'\s+', ' ', data[-1].strip()).split(' ')
 
     # Get lines as strings, transpose with row coordinate running backwards
     operands = []
     group = []
     for i in range(len(data[0])):
         digit = ""
-        for j in range(len(data) - 2, -1, -1):
+        for j in range(len(data) - 1):
             digit += data[j][i]
         digit = digit.strip()
         if digit != "":
-            group.append(int(digit[::-1]))
+            group.append(int(digit))
         else:
-            operands.append(deepcopy(group))
+            operands.append(group.copy())
             group = []
     operands.append(group)
 
     ret = 0
     for i in range(len(operators)):
-        ans = 0
-        if operators[i] == "*":
-            ans = 1
+        ans = 0 if operators[i] == "+" else 1
         for j in range(len(operands[i])):
-            if operators[i] == "*":
-                ans = ans * int(operands[i][j])
-            else:
+            if operators[i] == "+":
                 ans += int(operands[i][j])
+            else:
+                ans *= int(operands[i][j])
         
         ret += ans
 
